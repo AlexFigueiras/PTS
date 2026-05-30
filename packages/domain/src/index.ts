446c219a1a7e8f42b67d177a98bc6e0bc7fbb344
AlexFigueiras <diretoralexfigueiras@gmail.com>
2026-05-30 19:33:55 -0300

    c7: barrel index

--- packages/domain/src/index.ts
+++ packages/domain/src/index.ts
@@ -1,1 +1,8 @@
 export * from './dimensions';
+export * from './legacy-axis';
+export * from './sensitivity';
+export * from './errors';
+export * from './fsm';
+export * from './action';
+export * from './signal';
+
